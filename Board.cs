eaa00798c0b32fb1fe19fcb442b77aad069efdbc
Yorick Mackaaij <yorick1230@gmail.com>
2020-01-20 13:27:24 +0100

    fix conflicts

--- Board.cs
+++ Board.cs
@@ -1,12 +1,6 @@
 ﻿using Assets.scripts;
-<<<<<<< HEAD
 using Assets.scripts.Model;
 using System;
-=======
-using FenParser;
-using System;
-using System.Collections;
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -16,36 +10,14 @@ public class Board : MonoBehaviour
 {
     public static Board instance { set; get; }
     private bool[,] allowedMoves { set; get; }
-<<<<<<< HEAD
-=======
-
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
     public ChessPiece[,] chessPieces { set; get; }
     private ChessPiece selectedPiece;
 
     private const float TILE_SIZE = 1.0f;
     private const float TILE_OFFSET = 0.5f;
 
-<<<<<<< HEAD
     private int puzzleCount = 0;
     private List<string> currentMoves;
-=======
-    // Ik kon geen API vinden voor schaakpuzzels vandaar dat ik hier een paar puzzels heb gehardcode om het spel te demonstreren.
-    // parameters: FEN, isWhiteTurn, Moves
-    private readonly Puzzle[] FEN_PUZZLES = {
-        new Puzzle("1Q6/3rq1k1/p7/2p2pp1/4p3/P3P1P1/4bPN1/1R4K1 w - - 4 39", 
-            true, 
-            new string[]{ "b8b2", "e7f6", "b2e2", "f6c6"}),
-        new Puzzle("5R2/1b4b1/p1q3pk/1pp3N1/3p4/P1PPr3/BPRQ2PP/7K b - - 0 29",
-            false,
-            new string[]{"c6g2", "d2g2", "e3e1", "f8f1", "e1f1"}),
-        new Puzzle("5b1k/7P/6Q1/4p2n/5r2/2P5/6P1/2R3K1 b - - 2 49",
-            false,
-            new string[]{"f8c5","g1h2","f4h4"}),
-    };
-    private int currentPuzzle = 0;
-    private string[] currentMoves;
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
 
     private int selectionX = -1;
     private int selectionY = -1;
@@ -64,11 +36,7 @@ public class Board : MonoBehaviour
     private void Start()
     {
         instance = this;
-<<<<<<< HEAD
         SetupChessPuzzle();
-=======
-        SetupChessPuzzle(FEN_PUZZLES[currentPuzzle]);
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
     }
 
     // Update is called once per frame
@@ -101,7 +69,6 @@ public class Board : MonoBehaviour
         // if computer's turn
         if(isPlayerWhite && !isWhiteTurn || !isPlayerWhite && isWhiteTurn)
         {
-<<<<<<< HEAD
             if (currentMoves.Count > 0)
             {
                 // convert coordinates to numbers and make computer move
@@ -111,54 +78,24 @@ public class Board : MonoBehaviour
             }
 
             if (currentMoves.Count <= 0)
-=======
-            if (currentMoves.Length > 0)
-            {
-                // convert coordinates to numbers and make computer move
-                int fromX = 0, fromY = 0, targetX = 0, targetY = 0;
-                convertCoordinates(currentMoves[0], ref fromX, ref fromY, ref targetX, ref targetY);
-                SelectPiece(fromX, fromY);
-                MovePiece(targetX, targetY, true);
-            }
-
-            if (currentMoves.Length <= 0)
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
             {
                 // advance to next puzzle
                 puzzlesCompleted++;
                 puzzlesCompletedUi = GameObject.Find("PuzzleScore").GetComponent<Text>();
                 puzzlesCompletedUi.text = "Puzzles completed: " + puzzlesCompleted;
 
-<<<<<<< HEAD
                 puzzleCount++;
                 clearField();
                 SetupChessPuzzle();
-=======
-                if (currentPuzzle < FEN_PUZZLES.Length-1)
-                    currentPuzzle += 1;
-                else
-                {
-                    currentPuzzle = 0; // start from beginning
-                }
-                clearField();
-                SetupChessPuzzle(FEN_PUZZLES[currentPuzzle]);
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
                 return;
             }
         }
     }
 
-<<<<<<< HEAD
     // Selects a piece to move, returns void if no piece on given coordinates
     private void SelectPiece(int x, int y)
     {
         Debug.Log("Selected: x:" + x + " y:" + y);
-=======
-
-    // Selects a piece to move, returns void if no piece on given coordinates
-    private void SelectPiece(int x, int y)
-    {
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
         if (chessPieces[x, y] == null)
             return;
 
@@ -192,15 +129,9 @@ public class Board : MonoBehaviour
                 chessPieces[x, y] = selectedPiece;
                 isWhiteTurn = !isWhiteTurn;
 
-<<<<<<< HEAD
                 if(currentMoves.Count > 0)
                 {
                     currentMoves.RemoveAt(0); // remove first move, since its already played on the board
-=======
-                if(currentMoves.Length > 0)
-                {
-                    currentMoves = currentMoves.Skip(1).ToArray(); // remove first move, since its already played on the board
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
                 }
                 if(!computerMove)
                     Debug.Log("Correct!");
@@ -262,16 +193,11 @@ public class Board : MonoBehaviour
     }
 
     // sets up the board with the given puzzle
-<<<<<<< HEAD
     private void SetupChessPuzzle()
-=======
-    private void SetupChessPuzzle(Puzzle FEN_PUZZLE)
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
     {
         activePieces = new List<GameObject>();
         chessPieces = new ChessPiece[8, 8];
 
-<<<<<<< HEAD
         // Retrieve new puzzle
         HttpClient hc = new HttpClient();
         Puzzle ActivePuzzle = hc.retrieveRandomPuzzle();
@@ -290,13 +216,6 @@ public class Board : MonoBehaviour
         Debug.Log(String.Join(", ", currentMoves.ToArray()));
         currentMoves.Insert(0, ActivePuzzle.BlunderMove);
         
-=======
-        FenParser.FenParser parser = new FenParser.FenParser(FEN_PUZZLE.fen);
-        isWhiteTurn = FEN_PUZZLE.isPlayerWhite;
-        isPlayerWhite = FEN_PUZZLE.isPlayerWhite;
-        currentMoves = FEN_PUZZLE.correctMoves;
-
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
 
         moveColor = GameObject.Find("MoveColor").GetComponent<Text>();
         moveColor.text = isPlayerWhite ? "White to move" : "Black to move";
@@ -362,7 +281,6 @@ public class Board : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
     // Converts chess coordinates (e.g. Rxb4) to number coordinates used by the game (e.g. [1,2])
     private Coordinate convertCoordinates(string coord)
     {
@@ -506,43 +424,15 @@ public class Board : MonoBehaviour
     }
 
 
-=======
-    // Converts chess coordinates (e.g. e2e4) to number coordinates used by the game (e.g. [1,2])
-    private void convertCoordinates(string coord, ref int fromX, ref int fromY, ref int toX, ref int toY)
-    {
-        char[] columns = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
-
-        // convert move to x and y coordinates
-        string originCoordinate = coord.Substring(0, 2);
-        string targetCoordinate = coord.Substring(2, 2);
-
-        // convert letter to number
-        fromX = Array.IndexOf(columns, (char)originCoordinate[0]);
-        fromY = Int32.Parse(originCoordinate[1] + "") - 1;
-
-        toX = Array.IndexOf(columns, (char)targetCoordinate[0]);
-        toY = Int32.Parse(targetCoordinate[1] + "") - 1;
-    }
-
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
     // Checks if the users move is 'correct' (according to the puzzle, not legal move).
     private bool isCorrectMove(int fromX, int fromY, int toX, int toY)
     {
         // convert coordinates to numbers
-<<<<<<< HEAD
         Coordinate coord = convertCoordinates(currentMoves[0]); // get correct coordinates
 
         // compare coordinates
         if (coord.fromX == fromX && coord.fromY == fromY &&
             coord.toX == toX && coord.toY == toY)
-=======
-        int convertedFromX = 0, convertedFromY = 0, convertedTargetX = 0, convertedTargetY = 0;
-        convertCoordinates(currentMoves[0], ref convertedFromX, ref convertedFromY, ref convertedTargetX, ref convertedTargetY);
-
-        // compare
-        if (convertedFromX == fromX && convertedFromY == fromY &&
-            convertedTargetX == toX && convertedTargetY == toY)
->>>>>>> e47297ff8fe1f2847c811b35e37c104365f23046
         {
             return true;
         }
